d992ddb993b2d154e95ceba79ee4ec4077d2d6fc
Jonathan Wiguna <jonathan.wiguna1@gmail.com>
2022-07-24 00:04:35 +0800

    added records page

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 
 const port = process.env.PORT || 5000;
-// app.use(cors());
-
-//Cors Configuration - Start
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
-//Cors Configuration - End
-
+app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
